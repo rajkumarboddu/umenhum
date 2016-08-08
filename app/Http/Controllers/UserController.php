71a2a08af80fad382799a58e34b5d992e48c9427
Raj <rajkumar.freelancing@gmail.com>
2016-08-08 19:16:22 +0530

    get user name by referral code completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -179,6 +179,14 @@ class UserController extends Controller
         }
     }
 
+    public function getUserNameByReferralCode($referral_code){
+        $user = User::where('referral_code',$referral_code)->select('first_name')->first();
+        if($user){
+            return response()->json($user,200);
+        }
+        return response()->json('User not found with this referral code',404);
+    }
+
     public function doLogout(){
         Auth::logout();
         return redirect('/');
